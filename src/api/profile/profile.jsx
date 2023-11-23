aee3fc045ff76a98e9a240d88be12849333ffc60
alijon13 <alijonrasulov1337@gmail.com>
2023-11-23 18:19:17 +0500

    Profile-Design

--- src/api/profile/profile.jsx
+++ src/api/profile/profile.jsx
@@ -1,2 +1,15 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 
+
+export const getProfile = createAsyncThunk(
+    'profile/getProfile',
+    async function(){
+        try {
+            
+        } catch (error) {
+            
+        }
+    } 
+)
---
+++ src/components/Profile/Tagged.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+
+
+const Tagged = () => {
+
+
+
+
+
+  return (
+    <div className="">
+
+          
+      
+    </div>
+  
+    
+    
+
+
+  );
+};
+
+export default Tagged;
+
