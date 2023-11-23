3bbab02ebf1ef9319712d739825b8447f511410d
samarddin <figmacom4@gmail.com>
2023-11-23 16:39:55 +0300

    last

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,13 @@
-
-
-
-
 import { configureStore } from "@reduxjs/toolkit";
 import layout from "../reducers/Layout/Layout";
+import profile from "../reducers/Profile/Profile";
+import explore from "../reducers/explore/Explore";
 
 export const store = configureStore({
   reducer: {
     layout,
+    profile,
+    explore,
   },
 });
 
