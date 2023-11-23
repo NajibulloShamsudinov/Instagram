399de4c70ce999ec3e8803e2f71cd3ee1d4f437d
Nushofarin <nushahakimova03@gmail.com>
2023-11-23 19:34:29 +0500

    last

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import layout from "../reducers/Layout/Layout";
-import home from "../reducers/Home/Home";
-
 
 export const store = configureStore({
   reducer: {
     layout,
-    home
   },
 });
