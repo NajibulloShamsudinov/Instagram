265d62f72d6db09b4d4abc587bb8ba5f05af2f35
saidamir101 <saidamir101@gmail.com>
2023-11-23 19:15:53 +0500

    last

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import layout from "../reducers/Layout/Layout";
-import searchred from "../reducers/search/searchred";
 
 
 export const store = configureStore({
   reducer: {
-    layout,
-    searchred
+    layout, 
   },
 });
