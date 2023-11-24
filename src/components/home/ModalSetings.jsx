b5dedc49163ef9d8ca3d5f8e77414057ba04e420
Nushofarin <nushahakimova03@gmail.com>
2023-11-24 18:25:56 +0500

    me

---
+++ src/components/home/ModalSetings.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
+
+
+
+const style = {
+  rounded:20,
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 24,
+};
+
+const ModalSetings = ({open,close}) => (
+    <div>
+        <Modal
+            open={open}
+            close={close}
+            closeAfterTransition
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+                backdrop: {
+                    timeout: 500,
+                },
+            }}
+        >
+            <Fade in={open}>
+                <Box sx={style}>
+                    <ul className='text-center  '>
+                        <li className='text-[red] border-[2px] p-[10px]'>Пожаловатся</li>
+                        <li className='text-[red] border-[2px] p-[10px]'>Отменить подписку</li>
+                        <li className='border-[2px] p-[10px]'>Добавить в избранное</li>
+                        <li className='border-[2px] p-[10px]'>Перейти к публикатции</li>
+                        <li className='border-[2px] p-[10px]'>Поделиться...</li>
+                        <li className='border-[2px] p-[10px]'>Копировать ссылку</li>
+                        <li className='border-[2px] p-[10px]'>Всатавить на сайт</li>
+                        <li className='border-[2px] p-[10px]'>Об аккаунте</li>
+                        <li className='border-[2px] p-[10px]' >
+                        <button>Отмена</button>
+                        </li>
+                    </ul>
+                </Box>
+            </Fade>
+        </Modal>
+    </div>
+)
+
+export default ModalSetings
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { get } from "../../api/home/home";
+import { get, users,likes } from "../../api/home/home";
+import { handelChange, setOpen } from "../../reducers/Home/Home";
 import Checkbox from '@mui/material/Checkbox';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
-
-
-
+import Followers from "../../components/home/Followers";
+import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ModalSetings from "../../components/home/ModalSetings";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 function Home () {
+/////datas
   const data=useSelector(({home})=>home.data)
+  const user=useSelector(({home})=>home.user)
+  const open=useSelector(({home})=>home.open)
   const dispatch=useDispatch()
 
 
 useEffect(()=>{
   dispatch(get())
+  dispatch(users())
 },[dispatch])
 
 
@@ -25,42 +33,52 @@ useEffect(()=>{
     <div className="mx-[auto] p-[20px] pb-[10vh]">
       <div className="flex justify-between">
         <div className="w-[65%]">
-        <div className="flex gap-[10px] px-[40px]">
-         <div className="text-center">
-          <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-[30px] p-[2px]">
-          <img src="https://static.vecteezy.com/system/resources/previews/019/896/012/original/female-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png"
-          className="w-[50px] rounded-[30px] border-[2px] border-[white]"  alt="" />
-          </div>
-          <span className="text-[12px]">betman</span>
-         </div>
-         <div className="text-center">
-          <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-[30px] p-[2px]">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSinUiRqVB94sfZZbtNZgPJswUTs4R7YDskvXfVjUSejKfQqAoMaedQBNfybdIdduiix4&usqp=CAU"
-          className="w-[50px] rounded-[30px] border-[2px] border-[white]"  alt="" />
-          </div>
-          <span className="text-[12px]">betman</span>
-         </div>
-         <div className="text-center">
-          <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-[30px] p-[2px]">
-          <img src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
-          className="w-[50px] rounded-[30px] border-[2px] border-[white]"  alt="" />
-          </div>
-          <span className="text-[12px]">betman</span>
-         </div>
+        <div className="flex gap-[10px] mx-[40px]">
+        {
+          user.map((e)=>{
+            return (
+              <div className="text-center">
+              <div className="w-[60px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-[30px] p-[2px]">
+              <img src="https://cdn2.iconfinder.com/data/icons/instagram-ui/48/jee-75-512.png"
+              className="rounded-[30px] border-[2px] border-[white] bg-[white]"  alt="" />
+              </div>
+              <span className="text-[12px]">{e.userName}</span>
+             </div>
+            )
+          })
+        }
         </div>
         <div className="my-[10vh] ">
       {
-     data.map((e,i)=>{
+     data.map((e)=>{
       return (
-        <div key={e.postId} className="w-[400px] h-[500px] border-[1px] m-auto my-[20px]">
-        <img src={`${import.meta.env.VITE_APP_FILES_URL}${e.images[i]}`}
+        <div key={e.postId} className="w-[500px] h-auto m-auto my-[40px]">
+           <div className="flex justify-between">
+             <div  className="flex items-center gap-[4px]">
+             <div className="w-[40px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-[30px] p-[2px]">
+              <img src="https://cdn2.iconfinder.com/data/icons/instagram-ui/48/jee-75-512.png"
+              className="rounded-[30px] border-[2px] border-[white] bg-[white]"  alt="" />
+              </div>
+             <span className="text-[14px] font-semibold">admin</span>
+             </div>
+             <button onClick={()=>dispatch(setOpen(true))}>
+              <MoreVertIcon/></button>
+             </div> <br />
+        <img src={`${import.meta.env.VITE_APP_FILES_URL}${e.images[0]}`}
          className="w-[100%]" alt="error" />
         <div className="p-[2px]">
-        <div>
+          {console.log(`${import.meta.env.VITE_APP_FILES_URL}${e.images[0]}`)}
+        <div className="flex justify-between">
+         <div className="flex gap-[2px] items-center">
+        <div className="text-center">
         <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{color:"red"}} />}
          sx={{color:"red",":hover":{
           color:"red",
-        }}} />
+        }}} onClick={()=>dispatch(likes(e.postId))} /> <br />
+        {e.userLikes}
+        </div>
+        <ModeCommentOutlinedIcon/>
+         </div>
           <Checkbox
          icon={<BookmarkBorderIcon />}
            checkedIcon={<BookmarkIcon />}
@@ -73,32 +91,33 @@ useEffect(()=>{
      })
      }</div>
         </div>
+        {/* modals */}
+        <ModalSetings open={open} />
         
         {/* right side */}
        
-       <div className="mx-[50px]">
-       <div className="flex items-center gap-[10px]">
+       <div className="mx-[40px] w-[35%]">
+       <div className="flex items-center justify-between">
+          <div className="flex items-center gap-[10px]">
         <img src="https://static.vecteezy.com/system/resources/previews/019/896/012/original/female-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png"
-          className="w-[40px] rounded-[30px]"  alt="" />
+          className="w-[50px] rounded-[30px]"  alt="" />
           <div>
           <h1 className="text-[12px]">betman</h1>
           <h1 className="text-[12px]">noctua 😊</h1>
           </div>
-          <h1 className="text-[10px] text-[blue]">Переключиться</h1>
+          </div>
+          <h1 className="text-[14px] text-[blue]">Переключиться</h1>
          </div>
          <div className="text-[grey] my-[20px] text-[12px] flex justify-between">
          <h1>Рекомендации для вас</h1>
          <h1>Все</h1>
          </div>
-           <div className="flex items-center gap-[10px]">
-        <img src="https://icons-for-free.com/iconfiles/png/512/instagram+profile+user+icon-1320184028326496024.png"
-          className="w-[40px] rounded-[30px]"  alt="" />
-          <div>
-          <h1 className="text-[12px]">betman</h1>
-          <h1 className="text-[12px]">noctua 😊</h1>
-          </div>
-          <h1 className="text-[10px] text-[blue]">Подписаться</h1>
-         </div>
+           <Followers user="betman" img="https://i.pinimg.com/originals/02/af/6f/02af6f406b25e5c284daa0c39cf96a9a.jpg"/>
+           <Followers user="AMIRTJK" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThe-QcdiM2zDMaptVBPduk0oYeAw473xEy48VwSMBBA-Q0AbTRWcB6rxiGX70kPC9HVwM&usqp=CAU"/> 
+           <Followers user="damir" img="https://i.pinimg.com/236x/5f/5d/13/5f5d1320da50c29055bb0c1f12a1d777.jpg"/> <br />
+          <h1 className="text-[grey] text-[14px]">Информация Помощь Пресса API Вакансии Конфиденциальность Условия Места Язык
+          Meta Verified</h1><br />
+          <h1 className="text-[grey] text-[12px]">© 2023 INSTAGRAM FROM META</h1>
        </div>
       </div>
     </div>
