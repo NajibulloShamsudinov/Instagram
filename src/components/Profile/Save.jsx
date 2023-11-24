0508e0976ee6ab4e468e7a24009e95c30932a0a2
alijon13 <alijonrasulov1337@gmail.com>
2023-11-24 18:39:23 +0500

    navigation

--- src/components/Profile/Save.jsx
+++ src/components/Profile/Save.jsx
@@ -1,26 +1,14 @@
-import React, { useState } from "react";
-
+import React from 'react'
 
 
 const Save = () => {
-
-
-
-
-
   return (
-    <div className="">
-
-          
-      
+    <div className='flex flex-col m-auto my-[80px] items-center'>
+      <svg className="w-[15px] hover:text-[black]  pt-[3px] h-[15px]" aria-label="" class="x1lliihq x1n2onr6 x1roi4f4" fill="currentColor" height="60" role="img" viewBox="0 0 24 24" width="60"><title></title><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon></svg>
+      <h1 className='text-[30px] pt-[10px] font-[700]'>Нет сохранения</h1>
     </div>
-  
-    
-    
+  )
+}
 
-
-  );
-};
-
-export default Save;
+export default Save
 
