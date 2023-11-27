bc0e2d5e944ab8a1cfbe43b8d8d88ce0cf17cad5
saidamir101 <saidamir101@gmail.com>
2023-11-27 19:53:14 +0500

    me

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,30 +1,55 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosRequest } from "../../utils/axiosRequest";
-export const getdata=createAsyncThunk(
+import axios from "axios";
+export const getdata = createAsyncThunk(
     "search/getdata",
-    async function(_,{getState}){
-
-        let api2="User/get-users";
-        if(getState().searchred.searchinp.length!==0){
-
-            api2=`User/get-users?UserName=${getState().searchred.searchinp}`
-        }
+    async function (_, { getState }) {
         
 
+        let api2 = "User/get-users";
+        if (getState().searchred.searchinp.length !== 0) {
+            api2 = `User/get-users?UserName=${getState().searchred.searchinp}`
+        }
         try {
-            const {data}=await axiosRequest.get(api2)
+            const { data } = await axiosRequest.get(api2)
             return data.data
         } catch (error) {
             console.log(error);
         }
     }
+
 )
 
-export const deluser=createAsyncThunk(
-    "seacrh/deluser",
-    async function(id,{dispatch}){
+// export const storget=createAsyncThunk(
+//     "search/storget",
+//     async function(){
+//         try {
+//             const {datastor}=await axiosRequest.get("User/get-search-histories")
+           
+//             return datastor.data
+//             console.log(datastor);
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     }
+// )
+
+export const postuser=createAsyncThunk(
+    "search/postuser",
+    async  function(username,{dispatch}){
         try {
-            const {data}=await axiosRequest.delete(`User/delete-User?userId=${id}`)
+            const {data}=await axiosRequest.post(`SearchHistory/add-search-history?Text=${username}`)
+           dispatch(getdata)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+)
+export const deluser = createAsyncThunk(
+    "seacrh/deluser",
+    async function (id, { dispatch }) {
+        try {
+            const { data } = await axiosRequest.delete(`User/delete-User?userId=${id}`)
             dispatch(getdata())
         } catch (error) {
             console.log(error);
--- src/reducers/search/searchred.js
+++ src/reducers/search/searchred.js
@@ -5,30 +5,30 @@ import { getdata } from "../../pages/search/search";
 const searchred = createSlice({
   name: "searchred",
   initialState: {
-    data:[],
-    searchinp:"",
-    search:false
+    data: [],
+    searchinp: "",
+    search: false
   },
   reducers: {
 
-    handleChange:(state,action)=>{
-      state[action.payload.type]=action.payload.settype
-     },
-    
+    handleChange: (state, action) => {
+      state[action.payload.type] = action.payload.settype
+    },
+
   },
-  extraReducers:(builder)=>{
-    builder.addCase(getdata.pending,(state,action)=>{
+  extraReducers: (builder) => {
+    builder.addCase(getdata.pending, (state, action) => {
     });
-    builder.addCase(getdata.fulfilled,(state,action)=>{
-      state.data=action.payload
+    builder.addCase(getdata.fulfilled, (state, action) => {
+      state.data = action.payload
     })
-    builder.addCase(getdata.rejected,(state,action)=>{
+    builder.addCase(getdata.rejected, (state, action) => {
     })
   }
 
-  
 
-  
+
+
 });
 
 export default searchred.reducer;
