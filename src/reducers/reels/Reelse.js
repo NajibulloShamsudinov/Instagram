7e6ae19b6416f4eea7b759442ab68d90fef2f018
Isroil <hasanzodaisroil714@gmail.com>
2023-12-01 19:46:28 +0500

    isroil_1

--- src/reducers/reels/Reelse.js
+++ src/reducers/reels/Reelse.js
@@ -1,11 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getData, postLike, getData1, deleteComment } from "../../api/reels/Reels.js";
+import {
+  getData,
+  postLike,
+  getData1,
+  deleteComment,
+  addFollowing,
+} from "../../api/reels/Reels.js";
 
 const reels = createSlice({
   name: "reels",
   initialState: {
     posts: [],
-    users:[],
+    users: [],
     modal: false,
     modal2: false,
     loading: false,
@@ -24,45 +30,48 @@ const reels = createSlice({
       console.log(state.loading);
     });
     builder.addCase(getData.fulfilled, (state, action) => {
-       console.log(action);
+      console.log(action);
       state.posts = action.payload;
-     
     });
     builder.addCase(getData.rejected, (state, action) => {
       console.log(error);
     });
-        builder.addCase(getData1.pending, (state, action) => {
-          state.loading = true;
-          console.log(state.loading);
-        });
-        builder.addCase(getData1.fulfilled, (state, action) => {
-          console.log(action);
-          state.users = action.payload;
-        });
-        builder.addCase(getData1.rejected, (state, action) => {
-          console.log(error);
-        });
-     builder.addCase(postLike.pending, (state, action) => {
-      
-     });
-     builder.addCase(postLike.fulfilled, (state, action) => {
-       console.log(action);
-   
-     });
-     builder.addCase(postLike.rejected, (state, action) => {
-       console.log(error);
-     });
-    builder.addCase(deleteComment.pending, (state, action) => {
-      
-     });
-     builder.addCase(deleteComment.fulfilled, (state, action) => {
-        state.modal2=false
-   
-     });
-     builder.addCase(deleteComment.rejected, (state, action) => {
-       console.log(error);
-     });
-  
+    builder.addCase(getData1.pending, (state, action) => {
+      state.loading = true;
+      console.log(state.loading);
+    });
+    builder.addCase(getData1.fulfilled, (state, action) => {
+      console.log(action);
+      state.users = action.payload;
+    });
+    builder.addCase(getData1.rejected, (state, action) => {
+      console.log(error);
+    });
+    builder.addCase(postLike.pending, (state, action) => {});
+    builder.addCase(postLike.fulfilled, (state, action) => {
+      console.log(action);
+    });
+    builder.addCase(postLike.rejected, (state, action) => {
+      console.log(error);
+    });
+    builder.addCase(deleteComment.pending, (state, action) => {});
+    builder.addCase(deleteComment.fulfilled, (state, action) => {
+      state.modal2 = false;
+    });
+    builder.addCase(deleteComment.rejected, (state, action) => {
+      console.log(error);
+    });
+    // Подписчики
+
+    builder.addCase(addFollowing.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(addFollowing.fulfilled, (state, action) => {
+      state.subsciption = action.payload;
+    });
+    builder.addCase(addFollowing.rejected, (state, action) => {
+      state.loading = false;
+    });
   },
 });
 
